8bea93c62ea1e65f8d180ce6963ae4d9a260185d
Nguyen-Hoang-Bao-An <nguyenhoangbaoan12a13@gmail.com>
2023-09-12 00:47:27 +0700

    bắt lỗi

---
+++ src/main/java/groub2/backend/exception/CustomExceptionHandler.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package groub2.backend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+/**
+ *
+ * @author Ann
+ */
+@RestControllerAdvice
+public class CustomExceptionHandler {
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handlerNotFoundException(NotFoundException ex, WebRequest req) {
+        return new ErrorResponse(HttpStatus.NOT_FOUND, ex.getMessage());
+    }
+}
