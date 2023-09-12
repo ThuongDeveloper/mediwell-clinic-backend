14a21286492f2c4aaa3406ed7c6602a6642ef8e9
Khôi <nova1x1996@gmail.com>
2023-09-12 19:44:14 +0700

    sss

---
+++ src/main/java/groub2/backend/dto/DoctorWithRating.java
@@ -0,0 +1,125 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package groub2.backend.dto;
+
+import groub2.backend.entities.TypeDoctor;
+import java.util.Date;
+
+/**
+ *
+ * @author DELL
+ */
+public class DoctorWithRating {
+    private int id;
+    private String name;
+    private String username;
+    private String password;
+    private String email;
+    private String address;
+    private boolean gender;
+    private Date createAt;
+    private String role;
+    private String image;
+    private TypeDoctor typeDoctorId;
+    private Double averageRating; // Trường này chứa trung bình rating của bác sĩ
+
+    // getters và setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public TypeDoctor getTypeDoctorId() {
+        return typeDoctorId;
+    }
+
+    public void setTypeDoctorId(TypeDoctor typeDoctorId) {
+        this.typeDoctorId = typeDoctorId;
+    }
+
+    public Double getAverageRating() {
+        return averageRating;
+    }
+
+    public void setAverageRating(Double averageRating) {
+        this.averageRating = averageRating;
+    }
+}
