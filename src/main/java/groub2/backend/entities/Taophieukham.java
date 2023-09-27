60a75ee2c00a30abb37ac85223a635b65a92ff24
Nguyen-Hoang-Bao-An <nguyenhoangbaoan12a13@gmail.com>
2023-09-27 17:40:21 +0700

    no message

--- src/main/java/groub2/backend/entities/Taophieukham.java
+++ src/main/java/groub2/backend/entities/Taophieukham.java
@@ -4,6 +4,7 @@
  */
 package groub2.backend.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.Date;
@@ -28,6 +29,7 @@ import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 import org.hibernate.validator.constraints.Length;
+import org.springframework.format.annotation.DateTimeFormat;
 
 /**
  *
@@ -83,12 +85,13 @@ public class Taophieukham implements Serializable {
     @Column(name = "gender")
     private Boolean gender;
     @Column(name = "dob")
-    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dob;
     @Size(max = 250)
     @Column(name = "sympton")
     private String sympton;
     @OneToMany(mappedBy = "taophieukhamId")
+    @JsonIgnore
     private Collection<Toathuoc> toathuocCollection;
     @JoinColumn(name = "casher_id", referencedColumnName = "id")
     @ManyToOne
