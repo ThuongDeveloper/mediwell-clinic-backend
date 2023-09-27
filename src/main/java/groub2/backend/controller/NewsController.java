8a33d5da9207e11190d2d560b3bbb7f87ef43d47
ThuongDeveloper <hoangdeptraibodoiqua4321@gmail.com>
2023-09-28 02:21:30 +0700

    done

--- src/main/java/groub2/backend/controller/NewsController.java
+++ src/main/java/groub2/backend/controller/NewsController.java
@@ -5,10 +5,13 @@
 package groub2.backend.controller;
 
 import groub2.backend.entities.News;
+import groub2.backend.firebase.FirebaseImageService;
 import groub2.backend.service.NewsService;
+import java.io.IOException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +21,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
@@ -30,7 +35,9 @@ public class NewsController {
     @Autowired
     NewsService newsService;
    
-
+    @Autowired
+    FirebaseImageService _FirebaseImageService;
+    
     @GetMapping()
     @ResponseStatus(HttpStatus.OK)
     public List<News> listNews() {
@@ -47,11 +54,14 @@ public class NewsController {
         }
     }
 
-    @PostMapping
+    @PostMapping(value = "/create", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<News> addNews(@RequestBody News news) {
-        newsService.addNews(news);
-        return new ResponseEntity<>(news, HttpStatus.OK);
+    public ResponseEntity<String> addNews(@RequestPart News News, @RequestPart("file") MultipartFile file) throws IOException {
+         String urlIMG =  _FirebaseImageService.uploadImageNews(News, file);
+        News.setImage(urlIMG);
+        newsService.addNews(News);
+        
+        return new ResponseEntity<>("Success", HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
